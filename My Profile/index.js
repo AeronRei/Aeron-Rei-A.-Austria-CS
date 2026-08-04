54994614d755f92d7b060b1554f02a7f8bd5f828
AeronRei <aerzon9450@gmail.com>
2026-08-04 11:34:42 +0800

    Add files via upload

---
+++ My Profile/index.js
@@ -0,0 +1,9 @@
+let firstName = "Aeron";
+let lastName = "Austria";
+const imgSrc = "Eh.webp";
+
+document.getElementById("content").innerHTML = `
+<h1>My Profile</h1>
+<p>My name is ${firstName} ${lastName} </p>
+<img src="./${imgSrc}" alt="Profile Picture Goes Here" />
+`;
